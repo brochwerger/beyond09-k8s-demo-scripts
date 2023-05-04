5b5795c24dc3c17a045d091ac2bdb08f40452ed7
Benny Rochwerger <brochwer@redhat.com>
2023-05-04 15:10:34 +0300

    If TARGET=gke ask for additional parameters. Cleanup

--- setup.sh
+++ setup.sh
@@ -10,28 +10,47 @@ source $SCRIPT_DIR/functions.sh
 
 if [[ ! -f $SCRIPT_DIR/vars.sh ]] 
 then 
+
+    echo URL="http://0.0.0.0:9999" > $SCRIPT_DIR/vars.sh
+
     read -ei "demo" -p "BRANCH = " BRANCH
+    echo "BRANCH=$BRANCH" >> $SCRIPT_DIR/vars.sh
+
     read -ei "messageboard" -p "APPNAME = " APPNAME
+    echo "APPNAME=$APPNAME" >> $SCRIPT_DIR/vars.sh
+
     read -ei "quay.io" -p "REGISTRY = " REGISTRY
-    read -p "PROJECTID = " PROJECTID
-    read -ei "firefox" -p "BROWSER = " BROWSER
+    echo "REGISTRY=$REGISTRY" >> $SCRIPT_DIR/vars.sh
+
+    read -p "OWNERID = " OWNERID
+    echo "OWNERID=$OWNERID" >> $SCRIPT_DIR/vars.sh
+
+    read -ei "opera" -p "BROWSER = " BROWSER
+    echo "BROWSER=$BROWSER" >> $SCRIPT_DIR/vars.sh
 
     TARGET='?'
     until [[ $TARGET == "gke" ]] || [[ $TARGET == "minikube" ]]
     do
         read -ei "gke|minikube" -p "TARGET = " TARGET
     done 
-
-    echo "BRANCH=$BRANCH" > $SCRIPT_DIR/vars.sh
-    echo "APPNAME=$APPNAME" >> $SCRIPT_DIR/vars.sh
-    echo "REGISTRY=$REGISTRY" >> $SCRIPT_DIR/vars.sh
-    echo "PROJECTID=$PROJECTID" >> $SCRIPT_DIR/vars.sh
-    echo "BROWSER=$BROWSER" >> $SCRIPT_DIR/vars.sh
     echo "TARGET=$TARGET" >> $SCRIPT_DIR/vars.sh
-    echo URL="http://0.0.0.0:9999" >> $SCRIPT_DIR/vars.sh
+
+    if [[ $TARGET == "gke" ]] 
+    then 
+        read -ei "beyond09" -p "PROJECT = " PROJECT
+        echo "PROJECT=$PROJECT" >> $SCRIPT_DIR/vars.sh
+
+        read -ei "democluster" -p "CLUSTER = " CLUSTER
+        echo "CLUSTER=$CLUSTER" >> $SCRIPT_DIR/vars.sh
+
+        read -ei "us-central1" -p "REGION = " REGION
+        echo "REGION=$REGION" >> $SCRIPT_DIR/vars.sh
+    fi
 
 else
     source $SCRIPT_DIR/vars.sh
 fi
+
+exit
 
 
